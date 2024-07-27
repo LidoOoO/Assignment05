95e39c0ae172dcb1dd653b7ec754890b46b0cf90
[200~LidoOoO~ <lidom899@gmail.com>
2024-07-27 05:28:35 +0300

    Add project files.

---
+++ Assignment05/Program.cs
@@ -0,0 +1,358 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Assignment05
+{
+    internal class Program
+    {
+
+        #region 1.Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+
+        #region By Value
+
+        //static void SWAP(int x, int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        #endregion
+
+
+        #region By Reference
+
+        //static void SWAP(ref int x, ref int y)
+        //{
+        //    int temp = x;
+        //    x = y;
+        //    y = temp;
+        //}
+
+        #endregion
+
+
+
+        #endregion
+
+
+        #region 2.Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+
+        #region By Value 
+
+        //static void Array(int[] arr)
+        //{
+        //    if (arr != null)
+        //    {
+        //        arr[0] = 50;
+        //        for (int i = 0; i < arr.Length; i++)
+        //        {
+
+        //            Console.WriteLine("Func Array =====> {0} ", arr[i]);
+        //        }
+        //    }
+        //}
+
+        #endregion
+
+        #region By Reference
+
+        //static void Array(ref int[] arr)
+        //{
+        //    if (arr != null)
+        //    {
+        //        arr[0] = 50;
+        //        for (int i = 0; i < arr.Length; i++)
+        //        {
+
+        //            Console.WriteLine("Func Array =====> {0} ", arr[i]);
+        //        }
+        //    }
+        //}
+
+
+        #endregion
+
+        #endregion
+
+
+        #region 3.Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+        //static void SumSub(int a, int b, out int Sum, out int Sub)
+        //{
+        //    Sum = a + b;
+        //    Sub = a - b;
+        //}
+
+        #endregion
+
+
+        #region 4.Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+
+        //static int Count(int x)
+        //{
+
+        //    int sum = 0;
+
+        //    while (x > 0)
+        //    {
+        //        sum += x % 10;
+        //        x /= 10;
+        //    }
+
+        //    return sum;
+
+        //}
+
+
+        #endregion
+
+
+        #region 5.Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:
+
+
+        //static bool IsPrime(int x)
+        //{
+        //    if (x <= 1) return false;
+        //    if (x == 2) return true; 
+
+        //    int count = 0;
+        //    for (int i = 1; i <= x; i++)
+        //    {
+        //        if (x % i == 0)
+        //        {
+        //            count++;
+        //        }
+        //    }
+
+        //    if (count == 2) { return true; }
+
+        //    return false;
+        //}
+
+        #endregion
+
+
+        #region 6.Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+
+        //static void MinMaxArray(int[] arr , out int Max , out int Min)
+        //{
+        //    Max = arr[0];
+        //    Min = arr[0];
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if(arr[i] < Min)
+        //        {
+        //            Min = arr[i];
+        //        }
+
+        //        if (arr[i] > Max)
+        //        {
+        //            Max = arr[i];
+        //        }
+        //    }
+
+        //}
+
+        #endregion
+
+
+        #region 7.Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
+
+        //static long Fact(int n)
+        //{
+
+        //    long result = 1;
+
+        //    for (int i = 1; i <= n; i++)
+        //    {
+        //        result *= i;
+        //    }
+
+        //    return result;
+        //}
+
+        #endregion
+
+
+        #region 8.Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
+
+        //static string ChangeChar(string original, int position, char newChar)
+        //{
+        //    if (position < 0 || position >= original.Length)
+        //        throw new ArgumentOutOfRangeException(nameof(position), "Position is out of the string's range.");
+
+        //    char[] chars = original.ToCharArray();
+
+        //    chars[position] = newChar;
+
+        //    return new string(chars);
+        //}
+
+        #endregion
+
+        static void Main()
+        {
+
+            #region 1.Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+
+            // By Value  ===> It will not change or modify the original variable, it only controls itself.
+            // By Reference ===> It changes and modifies the original variable because it has its own reference.
+
+            //Example
+
+            #region By Value
+
+            //int A = 10, B = 20;
+            //Console.WriteLine("A = {0} \nB = {1}", A, B);
+            //SWAP(A, B);
+            //Console.WriteLine("============ After Swap ============");
+            //Console.WriteLine("A = {0} \nB = {1}", A, B);
+
+            #endregion
+
+            #region By Reference
+
+            //int A = 10, B = 20;
+            //Console.WriteLine("A = {0} \nB = {1}", A, B);
+            //SWAP(ref A, ref B);
+            //Console.WriteLine("============ After Swap ============");
+            //Console.WriteLine("A = {0} \nB = {1}", A, B);
+
+
+            #endregion
+
+            #endregion
+
+            #region 2.Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c# example.
+
+
+            /*
+             Both give the same result but with a different way of working
+
+             In the case of the value, the variable in the function has the reference to the array
+
+            But in the case of the reference, 
+            the variable in the function during its operation changes the name of the array to the name of the variable in the function,
+            meaning it is the same as the array,
+            and after the function ends,
+            the name of the array is returned 
+             */
+
+
+            //Example
+
+            #region By Value
+
+            //int[] Numbers = { 1, 2, 3, 4 };
+            //Console.WriteLine("Numbers Array ====> {0}", Numbers[0]);
+            //Console.WriteLine("======== After Edit ========");
+            //Array(Numbers);
+            //Console.WriteLine("Numbers Array ====> {0}", Numbers[0]);
+
+            #endregion
+
+            #region By Reference
+
+            //int[] Numbers = { 1, 2, 3, 4 };
+            //Console.WriteLine("Numbers Array ====> {0}", Numbers[0]);
+            //Console.WriteLine("======== After Edit ========");
+            //Array(ref Numbers);
+            //Console.WriteLine("Numbers Array ====> {0}", Numbers[0]);
+
+
+            #endregion
+
+
+
+            #endregion
+
+            #region 3.Write a c# Function that accept 4 parameters from user and return result of summation and subtracting of two numbers
+
+
+            //int a = 10, b = 5 , Sum , Sub;
+            //SumSub(a , b , out Sum , out Sub);
+            //Console.WriteLine("Sum Of Numbers ===> {0} \nSub Of Numbers ===> {1}" , Sum , Sub);
+
+            #endregion
+
+            #region 4.Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+
+            //int x;
+
+            //Console.WriteLine("Please Enter a Number");
+
+            //if (int.TryParse(Console.ReadLine(), out x))
+            //{
+            //    int res = Count(x);
+            //    Console.WriteLine("The sum of the digits of the number {0} is : {1}", x ,res);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid number.");
+            //}
+
+            #endregion
+
+            #region 5.Create a function named "IsPrime", which receives an integer number and retuns true if it is prime, or false if it is not:
+
+            //int x;
+
+            //Console.WriteLine("Please Enter a Number");
+
+            //if (int.TryParse(Console.ReadLine(), out x))
+            //{
+            //    bool res = IsPrime(x);
+            //    Console.WriteLine(res);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid number.");
+            //}
+
+
+            #endregion
+
+            #region 6.Create a function named MinMaxArray, to return the minimum and maximum values stored in an array, using reference parameters
+
+
+            //int[] Numbers = { 1, 2, 3, -5 , 0 , 504,  4, 5, 6 };
+            //int Max, Min;
+            //MinMaxArray(Numbers, out Max, out Min);
+            //Console.WriteLine("Max Of Array ===> {0} \nMin Of Array ===> {1}" , Max , Min);
+
+            #endregion
+
+            #region 7.Create an iterative (non-recursive) function to calculate the factorial of the number specified as parameter
+
+            //int x;
+
+            //Console.WriteLine("Please Enter a Number");
+
+            //if (int.TryParse(Console.ReadLine(), out x))
+            //{
+            //    long res = Fact(x);
+            //    Console.WriteLine(res);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid number.");
+            //}
+
+
+
+            #endregion
+
+            #region 8.Create a function named "ChangeChar" to modify a letter in a certain position (0 based) of a string, replacing it with a different letter
+
+            //string result = ChangeChar("hello", 1, 'a');
+
+            //Console.WriteLine(result); 
+
+
+
+            #endregion
+        }
+    }
+    
+}
---
+++ Demo/Demo.cs
@@ -0,0 +1,355 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+
+        #region Functions
+
+        public static void PrintShape()
+        {
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine("(*_*)");
+
+        }
+
+        public static void PrintShape(int count = 10, string pattern = "*_*")
+        {
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(pattern);
+
+        }
+
+
+        static void SWAP(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+
+        static int SumArray(params int[] arr)
+        {
+            int sum = 0;
+            if (arr != null)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                    sum += arr[i];
+            }
+            return sum;
+        }
+
+
+        static void SumMul(int x, int y, ref int Sum, ref int Mul)
+        {
+            Sum = x + y;
+            Mul = x * y;
+        }
+
+
+
+        #endregion
+        static void Main()
+        {
+            #region Boxing , UnBoxing
+
+            #region Ex01
+            //object O1;
+            //O1 = "Ahmed"; //Casting
+
+            //O1 = 1; //Casting [value type -> reference type] boxing
+
+            //O1 = 'a'; 
+            #endregion
+
+
+            #region Ex02
+
+            //int x = 10;
+            //object obj = x; //Boxing [Safe Casting]
+            ////obj = new object(10);
+
+
+            //object obj02 = 10;
+            //int y = (int)obj02; //Casting UnBoxing [UnSafe Casting]
+
+            //Console.WriteLine(y);
+
+            #endregion
+
+            #endregion
+
+            #region Nullable - Value Types
+
+            #region Value type 
+
+            #region Ex01
+
+            ////int x = null; // Invalid
+            //////int x allow only int numbers
+
+            ////int? y = null;// Valid
+            ////// int y allow int Values OR Null
+
+            //int? Y = null;
+            //int X = 10;
+            ////int? Z = Y; // Valid
+            ////int? Z = X; // Valid
+
+            //int? number = null; //nullable<int> Number;
+            //                    //Nullable -> Allow Null + Values
+
+
+            ////decimal Num01 = 1.1M;
+            ////decimal? Num02 = Num01; 
+
+            //decimal? Num01 = null;
+            //decimal Num02 = null;  
+            #endregion
+
+            #region Ex02
+
+            //int X = 10;
+
+            //int? Y = /*(int?)*/ X; //Valid Implicit Casting
+
+            //// Safe Casting
+
+            //int? Number01 = null;
+            //int  Number02 = (int)Number01; //Explicit Casting
+
+
+            //Console.WriteLine(Number02);
+            //// Unsafe Casting
+
+            #endregion
+
+            #region Ex03
+
+            //int? k = null;
+            //int l;
+            ////l = k; Unsafe
+            //if (k.HasValue)
+            //{
+            //    l = (int)k; //Casting
+            //}
+            //else
+            //{
+            //    l = 0;
+            //}
+
+            //l = k.HasValue ? k.Value : 0;
+            //l = k != null ? (int)k : 0;
+
+
+
+            #endregion
+
+            #endregion
+
+            #region Reference Type
+
+            //// Null is Default Value of Reference Type
+
+            #region Ex01
+
+            //string message = "Hiii";
+            //message = null; //Valid with Warning
+            //message = null!; // ! -> Null Forgiveness Operator
+
+            //string? message02 = null; // Valid without Warning
+
+            //Console.WriteLine(message02);
+            //Console.WriteLine(message); 
+            #endregion
+
+            #region Ex02
+
+            //string notNull = "hello";
+
+            //string? NullableString = default; // Null
+
+
+            //Console.WriteLine(NullableString);
+            //Console.WriteLine(notNull);
+
+            //// NullableString = notNull ; // Valid
+
+            //notNull = NullableString; //Valid With Warning
+
+            #endregion
+
+
+            #endregion
+
+            #region Null Propagation Operator ?
+
+            //int[] Numbers = default!; // Null
+            // NullReferenceException
+
+
+            //for(int i = 0; (Numbers != null) && (i < Numbers.Length) ; i++)
+            //for(int i = 0; i < Numbers?.Length; i++) 
+            //     Console.WriteLine(Numbers[i]);
+
+
+            //if(Numbers !=  null)
+            // {
+            //     for(int i = 0;i < Numbers.Length; i++)
+            //     {
+            //         Console.WriteLine(Numbers[i]);
+            //     }
+            // }
+
+            // Numbers != null
+            // i < Numbers.length
+
+            // Numbers?.length
+            // Numbers = null -> (Numbers?.length)=> Null
+            // Numbers != Null -> Numbers?.length =>  length
+
+            // ?
+
+            // ??
+
+            //int length = Numbers?.Length ?? 0;
+            #endregion
+
+            #endregion
+
+            #region Functions
+
+            //PrintShape(); Without Parameter
+
+            //PrintShape(20 , "/-"); // Passing Parameter With Order
+
+            //PrintShape("/-" , 20); // Invalid
+
+            //PrintShape(pattern:"/-" ,count: 20); // Pasing Parameter By Name
+
+            //PrintShape(); // Using Default  Values
+
+            //Hello To Route Welcome Waleed
+            //Console.WriteLine("Hello To Route \n Welcome \t Waleed");
+
+            // C:\Users\Waleed\source\repos\Assignment05\Assignment05.sln
+
+            //string path = @"C:\Users\Waleed\source\repos\Assignment05\Assignment05.sln";
+
+            //Console.WriteLine(path);
+
+
+            //PrintShape(10 , "*(\\");
+            #endregion
+
+            #region Passing Parameters
+
+            #region Passing Value Type Parameters
+
+            // int  , Double ,Char 
+
+            #region Passing By Value
+            //int a = 10 ,  b = 20;
+            //Console.WriteLine($"A = {a}");
+            //Console.WriteLine($"B = {b}");
+            //SWAP(a, b);
+            //Console.WriteLine("SWAP"); //Passing By Value
+
+            //Console.WriteLine($"A = {a}");
+            //Console.WriteLine($"B = {b}"); 
+            #endregion
+
+            #region Passing By Reference
+
+            //int a = 10, b = 20;
+            //Console.WriteLine($"A = {a}");
+            //Console.WriteLine($"B = {b}");
+            //SWAP(ref a, ref b);
+            //Console.WriteLine("SWAP"); //Passing By Reference
+
+            //Console.WriteLine($"A = {a}");
+            //Console.WriteLine($"B = {b}");
+
+            #endregion
+
+            #endregion
+
+            #region Passing Reference Type Parameters
+
+            #region Passing By Value
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(Numbers[0]);
+            //int res = SumArray(Numbers);
+            //Console.WriteLine(res);
+            //Console.WriteLine(Numbers[0]); 
+            #endregion
+
+            #region Passing By Ref
+            //int[] Numbers = { 1, 2, 3, };
+            //int Result = SumArray(ref Numbers); // Passing By Ref
+            //                                    //  Array Its Self
+            //Console.WriteLine(Result);
+            #endregion
+
+            #region Ex01 By Value
+
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(Numbers[0]);
+            //int Result = SumArray(Numbers); // Passing By Value
+            ////Address Of Array 
+            ////Arr = Numbers
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Result);
+
+
+            #endregion
+
+
+            #region Ex02 By reference
+
+
+            //int[] Numbers = { 1, 2, 3 };
+            //Console.WriteLine(Numbers[0]);
+            //int Result = SumArray(ref Numbers); // Passing By Value
+            ////Address Of Array 
+            ////Arr = Numbers
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Result);
+
+
+            #endregion
+
+
+            #endregion
+
+            #region Passing By Out
+
+            //int A = 10, B = 20;
+            //int SumRes = 0 , MulRes = 0 ;
+            ////SumMul(A, B ,out SumRes ,out MulRes); // Passing By Out
+            //SumMul(A, B ,ref SumRes ,ref MulRes); // Passing By Reference
+
+            //Console.WriteLine(SumRes);
+            //Console.WriteLine(MulRes);
+
+            #endregion
+
+            #region Params
+
+            //int[] Numbers = { 1, 2, 3, 4, 5, 6, 7 };
+            //int result = SumArray(1, 2, 3, 4, 5, 6, 7);
+            //Console.WriteLine(result);
+
+
+            //int a = 10 , b = 5 , c = 30 ,d = 45;
+            //string Name = "Test";
+
+            //Console.WriteLine("{0} + {1} + {3} = {2}  {4}" , a , b, d, c ,Name);
+
+            #endregion
+
+            #endregion
+
+        }
+    }
+}
